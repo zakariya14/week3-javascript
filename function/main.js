962356bb549530c9d1f1ea416c3d028b5a063727
Zakariya Febriansah <zakariyaindonesia@gmail.com>
2022-12-16 19:20:40 +0700

    tugas function selesai

--- function/main.js
+++ function/main.js
@@ -1,49 +1,78 @@
-let tanya = true;
-alert(`selamat datang di game tebak angka
-anda diminta untuk menebak angka 1 - 3
-kemudian anda akan bermain dalam 5 ronde
-player yang berhasil menebak angka terbanyak, akan menang
-SELAMAT BERMAIN!!!!`);
-while (tanya) {
-  // menangkap pilihan player
-  let p1 = prompt(`player 1 : masukkan angka 1 - 3`);
-  let p2 = prompt(`player 2 : masukkan angka 1 - 3`);
+// Membuat game tebak angka menggunakan prompt
 
-  // menangkap pilihan komputer
-  // membangkitkan bilangan random
-  let comp = Math.floor(Math.random() * 3) + 1;
-
-  console.log(comp);
-  if (comp == 1) {
-    comp = "1";
-  } else if (comp == 2) {
-    comp = "2";
+alert(`selamat datang di permainan tebak angka.
+anda diminta untuk menebak angka 1 - 3, dalam 5 ronde.
+ada 2 player dalam permainan ini.
+player yang berhasil menebak angka terbanyak akan menang. SELAMAT BERMAIN....!
+`);
+const hasilAkhir = (nilaiP1, nilaiP2) => {
+  if (nilaiP1 > nilaiP2) {
+    alert(`pemenangnya adalah Player 1`);
+  } else if (nilaiP2 > nilaiP1) {
+    alert(`pemenangnya adalah Player 2`);
   } else {
-    comp = "3";
+    alert(`hasilnya seri`);
   }
+};
+const tebakAngka = () => {
+  let tanya = true;
+  while (tanya) {
+    let nilaiP1 = 0;
+    let nilaiP2 = 0;
+    for (let i = 1; i <= 5; i++) {
+      // menangkap pilihan player
+      let p1 = parseInt(prompt(`Player 1 : Silahkan masukkan angka 1 - 3`));
+      let p2 = parseInt(prompt(`Player 2 : Silahkan masukkan angka 1 - 3`));
+      // menangkap pilihan komputer
+      // membangkitkan bilangan randoom
+      let comp = Math.floor(Math.random() * 3) + 1;
 
-  //   menentukan rules
-  let hasil = "";
-  if (p1 == p2) {
-    alert("hasil tidak boleh sama");
-  } else if (p1 == comp) {
-    hasil = `${p1} menang`;
-  } else if (p2 == comp) {
-    hasil = `${p2} menang`;
-  } else if (p1 && p2 !== comp) {
-    hasil = `tebakan salah`;
-  } else if (p1 && p2 <= 0) {
-    hasil = `tidak boleh kurang dari 1`;
-  } else if (p1 && p2 >= 4) {
-    hasil = `tidak boleh lebih dari 3`;
-  } else {
-    hasil = `tebakan tidak sesuai`;
+      // menentukan rules
+      if (p1 < 1 || p2 < 1) {
+        alert(`angka tidak boleh lebih kecil dari 1`);
+        i -= 1;
+        continue;
+      } else if (p1 > 3 || p2 > 3) {
+        alert(`angka tidak boleh lebih besar dari 3`);
+        i -= 1;
+        continue;
+      } else if (isNaN(p1) || isNaN(p2)) {
+        alert(`tidak boleh memasukkan selain angka`);
+        i -= 1;
+        continue;
+      } else if (p1 == p2) {
+        alert(`tebakan tidak boleh sama`);
+        i -= 1;
+        continue;
+      }
+
+      if (p2 == comp) {
+        alert(`tebakan player 2 benar`);
+        nilaiP2 += 1;
+      } else if (p1 == comp) {
+        alert(`tebakan player 1 benar`);
+        nilaiP1 += 1;
+      } else if (p1 != comp && p2 != comp) {
+        alert(`kedua player salah menebak.`);
+      }
+
+      // tampilkan hasilnya
+      alert(`Nilai yang dicari: ${comp}
+    - Player 1: ${nilaiP1} 
+    - Player 2: ${nilaiP2}`);
+      if (i == 5) {
+        hasilAkhir(nilaiP1, nilaiP2);
+      } else {
+        if (!confirm(`ronde ${i + 1}`)) {
+          hasilAkhir(nilaiP1, nilaiP2);
+        }
+      }
+    }
+
+    tanya = confirm(`lagi?`);
   }
+};
 
-  //   tampilkan hasil
-  alert(`hasil anda adalah:
-  ${p1} : ${hasil}
-  ${p2} : ${hasil}`);
+tebakAngka();
 
-  tanya = confirm(`lagi?`);
-}
+alert(`terima kasih`);
